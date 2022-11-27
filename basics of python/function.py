bd4039eb00d4f610ca74477d7c05359d5b136124
MohitSaini <mailtomohit2002@gmail.com>
2022-11-27 21:44:40 +0530

    Initial Commit

---
+++ basics of python/function.py
@@ -0,0 +1,4 @@
+def printname(name, phone_number):
+    print(name + str(phone_number))
+
+printname('Disha Sen',7848784541)
---
+++ basics of python/typecasting.py
@@ -0,0 +1,7 @@
+a=45.54
+b=int(a)
+c=str(a)
+print(b)
+print("The type of a :-",type(a))
+print("The type of b :-",type(b))
+print("The type of c :-",type(c))
